fd54eb2f5dec0c513b3e89fb90c359668182522b
Mukul <mukulbish98@gmail.com>
2019-03-22 00:19:20 +0530

    added sample ads instead of real ads.

--- app/src/main/java/com/xxluciferinxx/mukul/admob/MainActivity.java
+++ app/src/main/java/com/xxluciferinxx/mukul/admob/MainActivity.java
@@ -1,7 +1,7 @@
 package com.xxluciferinxx.mukul.admob;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,13 +14,14 @@ import com.google.android.gms.ads.reward.RewardedVideoAd;
 public class MainActivity extends AppCompatActivity {
 
     AdView adview;
-    Button but1,but2;
+    Button but1, but2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileAds.initialize(this,"ca-app-pub-5629116281807493~7279508232");
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
 
         final AdRequest adRequest = new AdRequest.Builder().build();
 
@@ -29,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
 
 
         final InterstitialAd interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId("ca-app-pub-5629116281807493/9815006380");
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
         interstitialAd.loadAd(adRequest);
 
         final RewardedVideoAd rewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
 
-        rewardedVideoAd.loadAd("ca-app-pub-5629116281807493/4382059766",adRequest);
+        rewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917", adRequest);
 
         but1 = findViewById(R.id.but1);
         but2 = findViewById(R.id.but2);
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 interstitialAd.loadAd(adRequest);
-                if (interstitialAd.isLoaded()){
+                if (interstitialAd.isLoaded()) {
                     interstitialAd.show();
                 }
                 interstitialAd.loadAd(adRequest);
@@ -52,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(rewardedVideoAd.isLoaded()){
+                if (rewardedVideoAd.isLoaded()) {
                     rewardedVideoAd.show();
                 }
-                rewardedVideoAd.loadAd("ca-app-pub-5629116281807493/4382059766",adRequest);
+                rewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917", adRequest);
             }
         });
     }
